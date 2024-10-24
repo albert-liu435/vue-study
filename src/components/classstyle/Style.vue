0936da673f222f05d54b20e519c496fbd0881aae
rookie <liuxili@bestseller.com.cn>
2024-10-24 13:23:32 +0800

    提那家classstyle

---
+++ src/components/classstyle/Style.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const appRef = ref({
+  // 字体是否倾斜
+  isItalic: false,
+  // 是否应用删除效果
+  isDelete: false,
+  classObj: {
+    italic: false,
+    delete: false,
+  },
+  // 高亮时的文本颜色
+  active: 'red',
+  // 文字的大小
+  fsize: 30,
+  // 背景颜色
+  bgcolor: 'pink'
+})
+
+</script>
+
+<style scoped>
+
+
+</style>
